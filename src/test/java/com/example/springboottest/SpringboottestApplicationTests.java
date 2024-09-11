7fd8b4378d7d6a77a1e0675f40f10ecf0f172e77
anyeok <https://github.com/anyeok>
2024-09-11 10:09:09 +0900

    article_list complete

--- src/test/java/com/example/springboottest/SpringboottestApplicationTests.java
+++ src/test/java/com/example/springboottest/SpringboottestApplicationTests.java
@@ -1,13 +1,25 @@
 package com.example.springboottest;
 
+import com.example.springboottest.article.Article;
+import com.example.springboottest.article.ArticleRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDateTime;
 
 @SpringBootTest
 class SpringboottestApplicationTests {
+	@Autowired
+	private ArticleRepository articleRepository;
 
 	@Test
 	void contextLoads() {
+		Article a1 = new Article();
+		a1.setTitle("테스트");
+		a1.setContent("테스트입니다.");
+		a1.setCreateDate(LocalDateTime.now());
+		this.articleRepository.save(a1);
 	}
 
 }
